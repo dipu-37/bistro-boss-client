fdd28bd84ae99b98b2a12c22e5a39954410ac7d7
dipu-37 <razibul1105nstu@gmail.com>
2024-09-26 00:19:55 +0600

    section component

--- src/Pages/Home/Catagori/Catagori.jsx
+++ src/Pages/Home/Catagori/Catagori.jsx
@@ -8,10 +8,16 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../Component/SectionTitle/SectionTitle';
 
 const Catagori = () => {
     return (
-        <div>
+        <section>
+            <SectionTitle subHeading={"---From 11:00am to 10:00pm---"}
+                heading={"ORDER ONLINE"}>
+                
+            </SectionTitle>
+           
             <Swiper
                 slidesPerView={4}
                 spaceBetween={30}
@@ -48,7 +54,9 @@ const Catagori = () => {
                 </SwiperSlide>
 
             </Swiper>
-        </div>
+     
+        </section>
+       
     );
 };
 
